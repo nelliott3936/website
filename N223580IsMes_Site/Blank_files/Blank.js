8c3f438439c1ec6ae221993e2b9ba3e90df35cea
Noah Elliott <noahelliott3222@gmail.com>
2025-12-22 17:59:15 -0800

    Add Mac Page

--- N223580IsMes_Site/Blank_files/Blank.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.2 local-build-20251222
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Blank_files/BlankMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id3');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
---
+++ N223580IsMes_Site/Macs_files/Macs.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.2 local-build-20251222
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Macs_files/MacsMoz.css')
+Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id1');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
