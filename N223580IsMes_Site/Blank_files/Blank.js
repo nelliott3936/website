e5d043cbd73561b635e8b4c6548fd95890659b68
Noah Elliott <noahelliott3222@gmail.com>
2025-12-24 09:20:13 -0800

    PUSH

---
+++ N223580IsMes_Site/Blank_files/Blank.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.2 local-build-20251224
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Blank_files/BlankMoz.css')
+Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id1');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
