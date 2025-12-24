814b27e56c9be731fff2a52759fdf5675795d2b4
Noah Elliott <noahelliott3222@gmail.com>
2025-12-24 09:23:06 -0800

    FIX

--- N223580IsMes_Site/Will_RSS_Work_files/Will_RSS_Work.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.2 local-build-20251223
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Will_RSS_Work_files/Will_RSS_WorkMoz.css')
-Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id1');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
